c7465a7a1a8a49e818dea653209b1217715f46b3
alexeiguriev <alexeiguriev1@gmail.comm>
2021-07-09 00:38:52 +0300

    Relationship update

--- UserApiSql/Models/UserRole.cs
+++ UserApiSql/Models/UserRole.cs
@@ -10,6 +10,8 @@ namespace UserApiSql.Models
     public class UserRole : BaseEntity<int>
     {
         public int UserId { get; set; }
+        public User User { get; set; }
         public int RoleId { get; set; }
+        public Role Role { get; set; }
     }
 }
