4024f3b953660492c059cca3edafbdbdf8fb70f9
alexeiguriev <alexeiguriev1@gmail.comm>
2021-08-13 00:04:06 +0300

    database initialization update

--- UserApiSql/Data/DbInitializer.cs
+++ UserApiSql/Data/DbInitializer.cs
@@ -13,7 +13,9 @@ namespace UserApiSql.Data
         {
             List<User> users = new List<User>()
             {
-                new User { FirstName = "Guriev", LastName = "Alexei", EmailAddress = "alexeiemail@Gmail.com", Password = "alexeiTestPassword" },
+                new User { FirstName = "Guriev", LastName = "Alexei", EmailAddress = "alexeiguriev1@gmail.com", Password = "alexeiTestPassword" },
+                new User { FirstName = "Grecu", LastName = "Marin", EmailAddress = "gm@gmail.com", Password = "gmPassword" },
+                new User { FirstName = "Milan", LastName = "Iurie", EmailAddress = "mi@Gmail.com", Password = "miPassword" },
                 new User { FirstName = "Carson", LastName = "Alexander", EmailAddress = "testemail@Gmail.com", Password = "TestPassword" },
                 new User { FirstName = "Popescu", LastName = "Ion", EmailAddress = "ionpopescumail@Gmail.com", Password = "ipPassword" }
             };
@@ -44,10 +46,11 @@ namespace UserApiSql.Data
         {
             List<Role> roles = new List<Role>()
             {
+                new Role{Name="Admin"},
+                new Role{Name="PM"},
                 new Role{Name="Developer"},
                 new Role{Name="Tester"},
-                new Role{Name="Reviewer"},
-                new Role{Name="PM"}
+                new Role{Name="Reviewer"}
             };
             foreach (Role role in roles)
             {
@@ -61,9 +64,10 @@ namespace UserApiSql.Data
             List<UserRole> userRoles = new List<UserRole>()
             {
                 new UserRole{UserId=1,RoleId=1},
-                new UserRole{UserId=1,RoleId=3},
                 new UserRole{UserId=2,RoleId=2},
-                new UserRole{UserId=3,RoleId=4}
+                new UserRole{UserId=3,RoleId=3},
+                new UserRole{UserId=4,RoleId=4},
+                new UserRole{UserId=5,RoleId=5}
             };
             foreach (UserRole userRole in userRoles)
             {
