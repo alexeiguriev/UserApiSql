78e00827ef8219cb88fe09c5d555e1614fb32274
alexeiguriev <gG4nmam85Z4aGpfZ>
2021-08-19 11:36:37 +0300

    Add Notification module

---
+++ UserApiSql/Models/Noti.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using UserApiSql.Models;
+
+namespace UserControlUI.ModelsDTO
+{
+    public class Noti : BaseEntity<int>
+    {
+        [Required]
+        public User FromUserId { get; set; }
+        [Required]
+        public User ToUserId { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string NotiHeader { get; set; } = "";
+        [Required]
+        [MaxLength(300)]
+        public string NotiBody { get; set; } = "";
+        public bool IsRead { get; set; } = false;
+        [Required]
+        [MaxLength(100)]
+        public string Url { get; set; } = "";
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [Required]
+        [MaxLength(100)]
+        public string Message { get; set; } = "";
+    }
+}
